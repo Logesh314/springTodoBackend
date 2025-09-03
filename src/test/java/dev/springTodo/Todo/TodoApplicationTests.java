155584f0fb122194faf405c72e82070a48292eb2
logesh kumar <logeshkumar1918@gmail.com>
2025-09-03 16:08:11 +0530

    first commit

---
+++ src/test/java/dev/springTodo/Todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.springTodo.Todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
